0bd80a633cff7258b84162be6022d0224325b5a6
jerome.zhou <jerome.zhou@nio.com>
2023-04-13 18:19:20 +0800

    [DenseTNT]:add train script & .gitignore

---
+++ script/train.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+OUTPUT_DIR=models.densetnt.1; \
+GPU_NUM=8; \
+python ../src/run.py --argoverse --future_frame_num 30 \
+  --do_train --data_dir train/data/ --output_dir ${OUTPUT_DIR} \
+  --hidden_size 128 --train_batch_size 64 --use_map \
+  --core_num 16 --use_centerline --distributed_training ${GPU_NUM} \
+  --other_params \
+    semantic_lane direction l1_loss \
+    goals_2D enhance_global_graph subdivide goal_scoring laneGCN point_sub_graph \
+    lane_scoring complete_traj complete_traj-3 \
